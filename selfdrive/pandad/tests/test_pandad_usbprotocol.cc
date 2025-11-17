e00f74626f49452926d043248e934f5061cf82f0
Thomas Wolfe <tomwolfe@gmail.com>
2025-11-17 00:41:05 -0600

    Fix CI build failures: segfaults in test compilation and model building
    - Fix potential memory issue in test_pandad_usbprotocol.cc by using proper
      kj::arrayPtr instead of kj::ArrayPtr with raw pointer
    - Add JIT_BATCH_SIZE=0 flag in CI environment to prevent segfaults
      during tinygrad model compilation

--- selfdrive/pandad/tests/test_pandad_usbprotocol.cc
+++ selfdrive/pandad/tests/test_pandad_usbprotocol.cc
@@ -41,7 +41,7 @@ PandaTest::PandaTest(uint32_t bus_offset_, int can_list_size, cereal::PandaState
     const std::string &dat = test_data[dlc_to_len[id]];
     can.setAddress(i);
     can.setSrc(util::random_int(0, 2) + bus_offset);
-    can.setDat(kj::ArrayPtr((uint8_t *)dat.data(), dat.size()));
+    can.setDat(kj::arrayPtr((const kj::byte*)dat.data(), dat.size()));
     total_pakets_size += sizeof(can_header) + dat.size();
   }
 
